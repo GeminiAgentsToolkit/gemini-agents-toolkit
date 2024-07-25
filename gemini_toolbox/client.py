0f3fef253035cd535efaec5f851fea4b003edc06
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2024-07-24 20:46:42 -0700

    history depth

--- gemini_toolbox/client.py
+++ gemini_toolbox/client.py
@@ -7,12 +7,14 @@ from gemini_toolbox import declarations
 
 class GeminiChatClient(object):
 
-    def __init__(self, functions, model, *, debug=False, recreate_client_each_time=True):
+    def __init__(self, functions, model, *, debug=False, recreate_client_each_time=False, history_depth=-1):
         self.functions = {func.__name__: func for func in functions}
         self.chat = model.start_chat()
         self._model = model
         self.debug = debug
         self.recreate_client_each_time = recreate_client_each_time
+        if history_depth > 0 and recreate_client_each_time:
+            raise ValueError("history_depth can only be used with recreate_client_each_time=False")
 
     def _call_function(self, function_call):
         if self.debug:
@@ -27,6 +29,23 @@ class GeminiChatClient(object):
             return func(**args)
         else:
             return {"error": "Function not found"}
+        
+    def _maybe_trim_history(self):
+        if self.history_depth > 0:
+            trimmed_history = []
+            user_messages_count = 0
+            
+            # Iterate through the history in reverse
+            for h in reversed(self.chat._history):
+                if h.role == "user":
+                    if user_messages_count < self.history_depth:
+                        trimmed_history.append(h)
+                        user_messages_count += 1
+                else:
+                    trimmed_history.append(h)
+                    
+            # Reverse the trimmed history to restore the original order
+            self.chat._history = list(reversed(trimmed_history))
         
     def _maybe_recreate_client(self):
         if self.recreate_client_each_time:
@@ -58,17 +77,17 @@ class GeminiChatClient(object):
         return response.text
     
 
-def generate_chat_client_from_functions_package(package, model_name="gemini-1.5-pro", *, system_instruction="", debug=False, recreate_client_each_time=True):
+def generate_chat_client_from_functions_package(package, model_name="gemini-1.5-pro", *, system_instruction="", debug=False, recreate_client_each_time=False, history_depth=-1):
     all_functions = [
         func
         for name, func in inspect.getmembers(package, inspect.isfunction)
     ]
     all_functions_tools = declarations.generate_tool_from_functions(all_functions)
     model = GenerativeModel(model_name=model_name, tools=[all_functions_tools], system_instruction=system_instruction)
-    return GeminiChatClient(all_functions, model, debug=debug, recreate_client_each_time=recreate_client_each_time)
+    return GeminiChatClient(all_functions, model, debug=debug, recreate_client_each_time=recreate_client_each_time, history_depth=history_depth)
 
 
-def generate_chat_client_from_functions_list(all_functions, model_name="gemini-1.5-pro", *, system_instruction="", debug=False, recreate_client_each_time=True):
+def generate_chat_client_from_functions_list(all_functions, model_name="gemini-1.5-pro", *, system_instruction="", debug=False, recreate_client_each_time=False, history_depth=-1):
     all_functions_tools = declarations.generate_tool_from_functions(all_functions)
     model = GenerativeModel(model_name=model_name, tools=[all_functions_tools], system_instruction=system_instruction)
-    return GeminiChatClient(all_functions, model, debug=debug, recreate_client_each_time=recreate_client_each_time)
+    return GeminiChatClient(all_functions, model, debug=debug, recreate_client_each_time=recreate_client_each_time, history_depth=history_depth)
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = read_requirements()
 
 setup(
     name='gemini_toolbox',
-    version='0.3.0',
+    version='0.4.0',
     packages=find_packages(),
     description='Toolbox For Using Gemini Agents SDK',
     long_description_content_type='text/markdown',
