d77c741175658f5f551849a8ae987cc0f20d3be1
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2024-09-02 14:37:06 -0700

    version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = read_requirements()
 
 setup(
     name='gemini_agents_toolkit',
-    version='2.0.2',
+    version='2.1.0',
     packages=find_packages(),
     description='Toolkit For Creating Gemini Based Agents',
     long_description_content_type='text/markdown',
