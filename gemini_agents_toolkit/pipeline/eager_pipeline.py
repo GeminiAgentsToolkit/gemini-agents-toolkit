0c85ce278dfee6d6fbbca8c20911264fdd5efaf6
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2024-09-11 08:50:17 -0700

    Update eager_pipeline.py

--- gemini_agents_toolkit/pipeline/eager_pipeline.py
+++ gemini_agents_toolkit/pipeline/eager_pipeline.py
@@ -1,37 +1,48 @@
 class EagerPipeline(object):
-    def __init__(self, agent, logger=None):
-        self.agent = agent
+    def __init__(self, *, default_agent=None, logger=None):
+        self.agent = default_agent
         self.prev_step_data = None
         self.logger = logger
 
 
-    def if_step(self, prompt, then_steps, else_steps):
+    def _get_agent(self, agent):
+        if agent:
+            return agent
+        if self.agent:
+            return self.agent
+        raise ValueError("either default agent or local(per ste) agent should be set")
+
+
+    def if_step(self, prompt, then_steps, else_steps, *, agent=None):
+        agent_to_use = self._get_agent(agent)
         if self.logger:
             self.logger.info(f"if_step: {prompt}, then_steps: {then_steps}, else_steps: {else_steps}") 
-        if self.boolean_step(prompt):
+        if self.boolean_step(prompt, agent=agent_to_use):
             if then_steps:
-                self.steps(then_steps)
+                self.steps(then_steps, agent=agent_to_use)
         else:
             if else_steps:
-                self.steps(else_steps)
+                self.steps(else_steps, agent=agent_to_use)
 
-    def steps(self, steps):
+    def steps(self, steps, *, agent=None):
+        agent_to_use = self._get_agent(agent)
         if isinstance(steps, list):
             for step in steps:
-                self.step(step)
+                self.step(step, agent=agent_to_use)
         else:
-            self.step(steps)
+            self.step(steps, agent=agent_to_use)
 
-    def step(self, prompt):
+    def step(self, prompt, *, agent=None):
         if self.logger:
             self.logger.info(f"step: {prompt}")
         prompt = f"""this is one step in the pipeline, this steps are user command but not comming direclty from the user:
         user prompt: {prompt}
         data from prev steps: {self.prev_step_data}"""
-        self.prev_step_data = self.agent.send_message(prompt)
+        agent_to_use = self._get_agent(agent)
+        self.prev_step_data = agent_to_use.send_message(prompt)
         return self.prev_step_data
     
-    def boolean_step(self, prompt):
+    def boolean_step(self, prompt, *, agent=None):
         if self.logger:
             self.logger.info(f"boolean_step: {prompt}")
         prompt = f"""this is one step in the pipeline, this steps are user command but not comming direclty from the user:
@@ -41,7 +52,8 @@ class EagerPipeline(object):
         data from prev steps: {self.prev_step_data}.
         
         IMPORTANT: remember you ONLY can return True/False"""
-        bool_answer = self.agent.send_message(prompt)
+        agent_to_use = self._get_agent(agent)
+        bool_answer = agent_to_use.send_message(prompt)
         if "true" in bool_answer.lower():
             if self.logger:
                 self.logger.info(f"boolean_step: True")
@@ -53,12 +65,13 @@ class EagerPipeline(object):
         else:
             raise ValueError("Invalid response from user, expected True/False only")
         
-    def summary(self):
+    def summary(self, *, agent=None):
         if self.logger:
             self.logger.info(f"summary")
         prompt = f"""Now if the final step of the pipeline/dialog, provide summary of main things that were done and why.
         do not omit any steps, and only print key details. This dialog was a pipline so do not assume user knows about
         any messages even if they were comming from the user before, now is the time to build proper summary for a user.
         Prev data from prev step: {self.prev_step_data}."""
-        return self.agent.send_message(prompt)
+        agent_to_use = self._get_agent(agent)
+        return agent_to_use.send_message(prompt)
     
