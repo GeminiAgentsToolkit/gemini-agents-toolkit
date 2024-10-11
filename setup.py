0bb676fbf85525ea0ac444a50cd04f4e6cd25175
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2024-10-11 10:52:19 -0700

    version 3.0.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ long_description = read_readme()
 
 setup(
     name='gemini_agents_toolkit',
-    version='2.4.1',
+    version='3.0.0',
     packages=find_packages(),
     description='Toolkit For Creating Gemini Based Agents',
     long_description_content_type='text/markdown',
