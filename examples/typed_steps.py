4da4376a7f3c6d28ae3334bffb260e0079f43257
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2024-10-24 21:01:43 -0700

    sync

---
+++ examples/typed_steps.py
@@ -0,0 +1,26 @@
+import vertexai
+from config import (PROJECT_ID, REGION, SIMPLE_MODEL)
+from gemini_agents_toolkit import agent
+from gemini_agents_toolkit.pipeline import Pipeline
+
+
+vertexai.init(project=PROJECT_ID, location=REGION)
+fs_agent = agent.create_agent_from_functions_list()
+
+pipeline = Pipeline(default_agent=fs_agent, use_convert_agent_helper=True)
+msg_, _ = pipeline.float_step("60 - 5% is?")
+
+print(msg_)
+
+msg_, _ = pipeline.int_step("60 - 5?")
+
+print(msg_)
+
+msg_, _ = pipeline.char_step("first character of the world 'hello' is?")
+
+print(msg_)
+
+msg_, _ = pipeline.string_array_step("sort words(by alphabet): 'my', 'hello', 'pen'")
+
+print(msg_)
+print(msg_[0])
