54364158e73f86d761cde2f8874c95ffc91bde1a
Nishant Sharma <nishants1994@gmail.com>
2024-12-27 11:35:38 -0800

    remove catching exception

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ long_description = read_readme()
 
 setup(
     name='gemini_agents_toolkit',
-    version='3.8.0',
+    version='3.8.1',
     packages=find_packages(),
     description='Toolkit For Creating Gemini Based Agents',
     long_description_content_type='text/markdown',
